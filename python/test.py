9f3e18734a64f024e562aad3aa4fe64f1ac0337b
Marcel Hacker <powerliftingmarcel@gmail.com>
2024-12-22 19:04:37 +0100

    Adjust test.py

--- python/test.py
+++ python/test.py
@@ -1,12 +1,18 @@
 # test opensim
-
 import struct 
 print("Bit version of python:",struct.calcsize("P") * 8)
+
+# activate your virtual environment 
+# cd penv
+# cd bin
+# source activate
 
 import opensim as osim
 
 model = osim.Model()
 
-print("Version and date:",osim.GetVersionAndDate())
+print("Version and date of opensim: ",osim.GetVersionAndDate())
 
 print("OpenSim model created successfully!")
+
+# solution was export PYTHONPATH=/Users/marcelhacker/Applications/OpenSim_4.5/sdk/Python:$PYTHONPATH
