f4c1352d4b180ca6995c97bfff2c0835e635febb
Marcel Hacker <powerliftingmarcel@gmail.com>
2024-12-22 20:34:40 +0100

    Add test plot in basics.py

--- python/basics.py
+++ python/basics.py
@@ -247,12 +247,17 @@ if __name__ == "__main__":
     #plt.plot(ik_sumo['hip_flexion_r'], ik_conv['hip_flexion_r'])
 
     # loop through the subplots and plot random data
-    for i in range(5):
-      for j in range(3):
+    for i in range(1):
+      for j in range(1):
             # test plot_curves
             current_dir = os.path.dirname(os.path.abspath(__file__))
-            filepath1 = os.path.join(current_dir, 'csv1.csv')
+            filepath1 = os.path.join('/Users/marcelhacker/Documents/opensim-deadlift-techniques/athlete_0_increased_force_3/sumo_dl_80kg02/', 'csv1.csv')
             print(filepath1)
-            # filepath2 = os.path.join(current_dir, 'csv2.csv')
             
+    #  for i in range(5):
+    #   for j in range(3):
+            # test plot_curves
+     #       current_dir = os.path.dirname(os.path.abspath(__file__))
+      #      filepath1 = os.path.join(current_dir, 'csv1.csv')
+      #      filepath2 = os.path.join(current_dir, 'csv2.csv')
 # END
