6e2f54b1b889b55956d96b6535083c0896b77254
Marcel Hacker <powerliftingmarcel@gmail.com>
2024-12-30 22:13:09 +0100

    Export total force mean variables

--- python/imports.py
+++ python/imports.py
@@ -5,7 +5,7 @@ import pandas as pd
 structure:
 1. class athlete
 2. athletes initialisation
-3. IK, ID and SO file definitions
+3. IK, ID and SO files definitions
 4. Time normalisation 
 5. Muscle group force sums
 6. Mean value definitions
@@ -70,17 +70,16 @@ ik_conv_2 = pd.read_csv(file_paths["ik_conv_path_2"], sep="\t", skiprows=10)
 ik_conv_3 = None
 # ID sumo
 id_sumo_1 = pd.read_csv(file_paths["id_sumo_path_1"], sep="\t", skiprows=6)
-id_conv_2 = pd.read_csv(file_paths["id_conv_path_2"], sep="\t", skiprows=6)
 
 # ID conv
+id_conv_2 = pd.read_csv(file_paths["id_conv_path_2"], sep="\t", skiprows=6)
+
 # SO sumo
 muscleForces_sumo_emptybar_0 = pd.read_csv(
     file_paths["muscle_forces_sumo_emptybar_path_0"], sep="\t", skiprows=14
 )
 muscleForces_sumo_0 = None
-# pd.read_csv(
-#   file_paths["muscle_forces_sumo_path_0"], sep="\t", skiprows=14
-# )
+
 muscleForces_sumo_1 = pd.read_csv(
     file_paths["muscle_forces_sumo_path_1"], sep="\t", skiprows=14
 )
@@ -91,24 +90,21 @@ muscleForces_conv_2 = pd.read_csv(
     file_paths["muscle_forces_conv_path_2"], sep="\t", skiprows=14
 )
 
-# SO conv
-## to do fix moment arms
 # print("\n PATH: ", file_paths["moment_arms_hip_flexion_r_sumo_path_1"])
 # athlete0_scaled_increased_force_3_MuscleAnalysis_Moment_hip_flexion_r.sto
 # athlete_0_scaled_increased_force_3_MuscleAnalysis_Moment_hip_flexion_r.sto
-# momentArms_sumo = pd.read_csv(
-#   file_paths["moment_arms_hip_flexion_r_sumo_path_1"], sep="\t", skiprows=0
-# )
-# print("\n momentArms sumo: ", momentArms_sumo.columns)
+momentArms_sumo = None
+
+##################################################################################################
 
 ### time normalise everything to 101 values
+ik_sumo_emptybar_time_normalised_0 = time_normalise_df(ik_sumo_emptybar_0)
 ik_sumo_time_normalised_1 = time_normalise_df(ik_sumo_1)
 ik_conv_time_normalised_1 = time_normalise_df(ik_conv_1)
 ik_conv_time_normalised_2 = time_normalise_df(ik_conv_2)
 id_sumo_time_nomalised_1 = time_normalise_df(id_sumo_1)
 id_conv_time_normalised_2 = time_normalise_df(id_conv_2)
-ik_sumo_emptybar_time_normalised_0 = time_normalise_df(ik_sumo_emptybar_0)
-# muscleForces_sumo_time_normalised_0 = time_normalise_df(muscleForces_sumo_0)
+muscleForces_sumo_time_normalised_0 = None  # time_normalise_df(muscleForces_sumo_0)
 muscleForces_sumo_time_normalised_1 = time_normalise_df(muscleForces_sumo_1)
 muscleForces_conv_time_normalised_1 = time_normalise_df(muscleForces_conv_1)
 muscleForces_conv_time_normalised_2 = time_normalise_df(muscleForces_conv_2)
@@ -233,6 +229,7 @@ total_conv_force_2 = sum_muscle_forces(
 )
 ##################################################################################################
 ## MEAN VARIABLES
+# IMPORTANT: only calculate the mean with the time normalised vars
 
 ik_conv_mean = get_mean_trail_values(
     ik_conv_time_normalised_1,  # have to be time normalised
@@ -242,6 +239,7 @@ ik_conv_mean = get_mean_trail_values(
 muscle_forces_conv_mean = get_mean_trail_values(
     muscleForces_conv_time_normalised_1,  # have to be time normalised
     muscleForces_conv_time_normalised_2,  # have to be time normalised
+    ## todo add trail 3
 )
 # calcute mean of array values get from muscle force sums
 buffer_mean_total_conv = [0] * 101  # buffer
