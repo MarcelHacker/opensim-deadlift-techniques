b0ca1ba844a24e2bbafe3437402347e3f2f39a78
Marcel Hacker <powerliftingmarcel@gmail.com>
2025-01-11 00:22:37 +0100

    Add result ik athlete_0

--- python/src/kinematicsPlot.py
+++ python/src/kinematicsPlot.py
@@ -1,8 +1,13 @@
 from src.imports import (
     plt,
+    athlete_0_parsed,
     athlete_0_ik_sumo_time_normalised_1,
     athlete_0_ik_sumo_time_normalised_2,
+    athlete_0_ik_conv_time_normalised_1,
+    athlete_0_ik_conv_time_normalised_2,
 )
+
+active_athlete_json = athlete_0_parsed
 
 
 def run_kinematics_plot(bool):
@@ -11,38 +16,67 @@ def run_kinematics_plot(bool):
         try:
             # create figure with 6x3 subplots (1 for sumo and 1 for conventional)
             rows = 3
-            cols = 3
-            fig, axs = plt.subplots(rows)
+            cols = 2
+            fig, axs = plt.subplots(cols, rows)
             fig.suptitle(
                 "Kinematics Trails "
-                + "athletes[0].name"
+                + active_athlete_json["name"]
                 + "; Model: "
-                + "athletes[0].model"
+                + active_athlete_json["model_name"]
                 + "; Preferred: "
-                + "athletes[0].technique"
+                + active_athlete_json["technique"],
+                fontweight="bold",
             )
             fig.set_label("Muscle Forces R")
             x_label = "% concentric deadlift cycle"
 
-            coordinates = [
+            coordinates_r = [
                 "hip_flexion_r",
                 "knee_angle_r",
                 "ankle_angle_r",
             ]
-            ylabels = ["Hip Flex [°]", "Knee Flex [°]", "Ankle Flex [°]"]
-            x = 0
-            ## kinematics of hip, knee and ankle, and muscle forces
-            for i in range(len(coordinates)):
-                print(i)
-                plt.sca(axs[i])
-                print("hi")
+            coordinates_l = [
+                "hip_flexion_l",
+                "knee_angle_l",
+                "ankle_angle_l",
+            ]
+            ylabels = [
+                "Hip Flex [°]",
+                "Knee Flex [°]",
+                "Ankle Flex [°]",
+            ]
+
+            for i in range(len(coordinates_r)):
+                plt.sca(axs[0, i])
+                plt.title("Sumo Deadlift")
                 plt.plot(
-                    athlete_0_ik_sumo_time_normalised_1[coordinates[i]],
+                    athlete_0_ik_sumo_time_normalised_1[coordinates_r[i]],
                     label="Trail 1",
                     color="red",
                 )
                 plt.plot(
-                    athlete_0_ik_sumo_time_normalised_2[coordinates[i]],
+                    athlete_0_ik_sumo_time_normalised_2[coordinates_r[i]],
+                    label="Trail 2",
+                    color="blue",
+                )
+                #  plt.plot(
+                #         athlete_0_ik_sumo_2["hip_flexion_r"],
+                #        label="Conventional 80%",
+                # )
+                plt.legend()
+                plt.ylabel(ylabels[i])
+                plt.xlabel(x_label)
+
+            for i in range(len(coordinates_l)):
+                plt.sca(axs[1, i])
+                plt.title("Conventional Deadlift")
+                plt.plot(
+                    athlete_0_ik_conv_time_normalised_1[coordinates_l[i]],
+                    label="Trail 1",
+                    color="red",
+                )
+                plt.plot(
+                    athlete_0_ik_conv_time_normalised_2[coordinates_l[i]],
                     label="Trail 2",
                     color="blue",
                 )
