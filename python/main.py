feb849b4b7dce18400c63aa214170001e3388abe
Marcel Hacker <powerliftingmarcel@gmail.com>
2025-01-11 00:32:45 +0100

    Add result ik athlete_0

--- python/main.py
+++ python/main.py
@@ -4,12 +4,14 @@
 from src.imports import *  # file paths of the athlete
 from src.local_functions import *  # local functions for the project
 from src.kinematicsPlot import run_kinematics_plot
+from src.momentsPlot import run_moments_plot
 
 #####################################################################################
 
 if __name__ == "__main__":
 
-    run_kinematics_plot(True)
+    run_kinematics_plot(False)
+    run_moments_plot(True)
 
     # increase_max_isometric_force(
     #   "/Users/marcelhacker/Documents/opensim-deadlift-techniques/athlete_1_increased_force_4/athlete_1_scaled.osim",
