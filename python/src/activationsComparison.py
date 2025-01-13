c63f4c4c89eb15392dcb9c5f3b1e3321bb0c3632
Marcel Hacker <powerliftingmarcel@gmail.com>
2025-01-13 22:12:46 +0100

    add emg data in plot

--- python/src/activationsComparison.py
+++ python/src/activationsComparison.py
@@ -15,13 +15,13 @@ def run_activations_comparison_from_emg(bool):
     if bool:
         try:
             # create figure with 6x3 subplots (1 for sumo and 1 for conventional)
-            rows = 5
+            rows = 6
             cols = 2
             color_emg = "red"
             color_computed = "blue"
             fig, axs = plt.subplots(cols, rows)
             fig.suptitle(
-                "Activations Trials "
+                "Activations Trial CONV 0; "
                 + active_athlete["name"]
                 + "; Model: "
                 + active_athlete["model"]
@@ -32,54 +32,93 @@ def run_activations_comparison_from_emg(bool):
             fig.set_label("Activations")
             x_label = "% concentric deadlift cycle"
 
-            coordinates = [
+            coordinates_r = [
                 "semiten_r",
                 "glmax2_r",
                 "recfem_r",
                 "vaslat_r",
                 "addmagIsch_r",
+                "tibant_r",
+            ]
+
+            coordinates_l = [
+                "semiten_l",
+                "glmax2_l",
+                "recfem_l",
+                "vaslat_l",
+                "addmagIsch_l",
+                "tibant_l",
             ]
 
             ylabels = [
-                "Hip Flex [°]",
-                "Knee Flex [°]",
-                "Ankle Flex [°]",
+                "semiten",
+                "glmax2",
+                "recfem",
+                "vaslat",
+                "addmagIsch",
+                "tibant",
             ]
-            for i in range(len(coordinates)):
-                current_muscle = coordinates[i]
-                print("currnt_muscle: ", coordinates[i])
+            for i in range(len(coordinates_r)):
+                current_muscle = coordinates_r[i]
                 plt.sca(axs[0, i])
-                plt.title("Conventional Deadlift")
+                plt.title("Conventional Deadlift R")
                 plt.plot(
                     active_athlete_activations_conv_time_normalised_0[current_muscle],
                     label="COMPUTED",
                     color=color_computed,
                 )
-                print(active_athlete_activations_conv_time_normalised_0[current_muscle])
-                print(
-                    active_athlete_activations_emg_conv_time_normalised_0[
-                        "EMG Channels_EMG15"
-                    ]
-                )
-                print(type(active_athlete_activations_emg_conv_time_normalised_0))
-                current_channel = active_athlete_emg_channels_order[current_muscle]
-                print("Current channel: ", current_channel)
-                print(
-                    "EMG: ",
-                    active_athlete_activations_emg_conv_time_normalised_0[0],
-                )
-                plt.plot(
-                    active_athlete_activations_emg_conv_time_normalised_0[
-                        str(current_channel)
-                    ],
-                    label="EMG",
-                    color=color_emg,
-                    linestyle="dashed",
-                )
-                #  add third trail
+                # Durchlaufen der Liste und Abrufen der Werte
+                for item in active_athlete_emg_channels_order:
+                    for key, value in item.items():
+                        # print(f"{key}: {value}")
+                        if key == current_muscle:
+                            curve = (
+                                active_athlete_activations_emg_conv_time_normalised_0[
+                                    value
+                                ]
+                                / 1000
+                            )
+                            plt.plot(
+                                curve,
+                                label="EMG",
+                                color=color_emg,
+                                linestyle="dashed",
+                            )
+
                 plt.legend()
                 plt.ylabel(ylabels[i])
                 plt.xlabel(x_label)
+
+            for j in range(len(coordinates_l)):
+                current_muscle = coordinates_l[j]
+                plt.sca(axs[1, j])
+                plt.title("Conventional Deadlift L")
+                plt.plot(
+                    active_athlete_activations_conv_time_normalised_0[current_muscle],
+                    label="COMPUTED",
+                    color=color_computed,
+                )
+                # Durchlaufen der Liste und Abrufen der Werte
+                for item in active_athlete_emg_channels_order:
+                    for key, value in item.items():
+                        if key == current_muscle:
+                            curve = (
+                                active_athlete_activations_emg_conv_time_normalised_0[
+                                    value
+                                ]
+                                / 1000
+                            )
+                            plt.plot(
+                                curve,
+                                label="EMG",
+                                color=color_emg,
+                                linestyle="dashed",
+                            )
+
+                plt.legend()
+                plt.ylabel(ylabels[j])
+                plt.xlabel(x_label)
+
             plt.show()
 
         except Exception as e:
