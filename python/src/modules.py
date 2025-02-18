774904283f73137da12c756c6879deace715b6fc
Marcel Hacker <powerliftingmarcel@gmail.com>
2025-02-18 19:40:03 +0100

    Add requirements file

--- python/src/modules.py
+++ python/src/modules.py
@@ -13,6 +13,25 @@ dir_name = os.path.dirname(os.path.dirname(current_directory))
 dir_name = "/Users/marcelhacker/Documents/opensim-deadlift-techniques/simulations"  # your dircetory
 
 
+def increase_max_isometric_force(model_path, factor):  # opensim API
+    print(model_path)
+
+    # Load the OpenSim model
+    model = osim.Model(model_path)
+
+    # Loop through muscles and update their maximum isometric force
+    for muscle in model.getMuscles():
+        current_max_force = muscle.getMaxIsometricForce()
+        new_max_force = current_max_force * factor
+        muscle.setMaxIsometricForce(new_max_force)
+
+    # Save the modified model
+    output_model_path = model_path.replace(".osim", f"_increased_force_{factor}.osim")
+    model.printToXML(output_model_path)
+
+    print(f"Model with increased forces saved to: {output_model_path}")
+
+
 def get_paths_athlete(athlete, model_name=None):
     # todos include moment arm paths in the emptybar trails, not only in the weighted trails
     # todos recheck moment arms paths, and add moment arms to conv
